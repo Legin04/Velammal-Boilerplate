e8ac4cdb7b3964c9914c87ca610ade098ad67f33
Lejin_ <114572345+Legin04@users.noreply.github.com>
2022-11-04 19:23:27 +0530

    Updated css

--- login.js
+++ login.js
@@ -1,28 +1,44 @@
 console.clear();
 
-const loginBtn = document.getElementById('login');
-const signupBtn = document.getElementById('signup');
+import firebase from "firebase/app";
 
-loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
+const firebaseConfig = {
+  apiKey: "AIzaSyApJFatbxamgzVSw3uMtjsBrlXVce6YOoA",
+  authDomain: "vhackathon-27e8e.firebaseapp.com",
+  databaseURL: "https://vhackathon-27e8e-default-rtdb.firebaseio.com",
+  projectId: "vhackathon-27e8e",
+  storageBucket: "vhackathon-27e8e.appspot.com",
+  messagingSenderId: "421517765892",
+  appId: "1:421517765892:web:0efcb22725b6a104313451",
+  measurementId: "G-M75BJ0M1WW",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
+
+loginBtn.addEventListener("click", (e) => {
+  let parent = e.target.parentNode.parentNode;
+  Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add("slide-up");
+    } else {
+      signupBtn.parentNode.classList.add("slide-up");
+      parent.classList.remove("slide-up");
+    }
+  });
 });
 
-signupBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode;
-	Array.from(e.target.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			loginBtn.parentNode.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
+signupBtn.addEventListener("click", (e) => {
+  let parent = e.target.parentNode;
+  Array.from(e.target.parentNode.classList).find((element) => {
+    if (element !== "slide-up") {
+      parent.classList.add("slide-up");
+    } else {
+      loginBtn.parentNode.parentNode.classList.add("slide-up");
+      parent.classList.remove("slide-up");
+    }
+  });
 });
